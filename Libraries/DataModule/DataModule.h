eff6425ab8937f908e4dac54a7e62dcedd4502a4
erlandss <91255495+erlandss@users.noreply.github.com>
2023-03-28 20:33:38 +0100

    Fixed minor error

---
+++ Libraries/DataModule/DataModule.h
@@ -0,0 +1,24 @@
+#ifndef DATAMODULE_H
+#define DATAMODULE_H
+
+#include "Arduino.h"
+#include "Filter.h"
+
+class DataModule
+{
+private:
+    //Private member variables:
+    uint8_t nodes[3];
+    Filter filter{0.70}; 
+public:
+    //Public member variables for easy access:
+    float coupling_gains[3] = {0};
+    //Public member functions:
+    DataModule(){};
+    ~DataModule(){};
+    void init(uint8_t* nodes_);
+    int at_node(uint8_t node);
+    void update_gains(uint8_t node, int adc_val);
+};
+
+#endif
