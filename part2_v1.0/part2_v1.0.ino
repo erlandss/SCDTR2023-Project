c8a0e4d14de9e1e2e19a487da6ccc92aad105fae
erlandss <91255495+erlandss@users.noreply.github.com>
2023-03-29 14:10:55 +0100

    Newest version

--- part2_v1.0/part2_v1.0.ino
+++ part2_v1.0/part2_v1.0.ino
@@ -3,6 +3,7 @@
 #include <mcp2515.h>
 #include "Timer.h"
 #include "DataModule.h"
+#include "ControlModule.h"
 
 const int DAC_RANGE = 4096;
 
@@ -12,10 +13,13 @@ const int LDR_PIN = A0;
 
 //Modules:
 DataModule data;
+ControlModule control;
 
-MCP2515 can0 {spi0, 17, 19, 16, 18, 10000000}; 
+//Timers for interrupts:
+struct repeating_timer local_control_timer;
+//struct repeating_timer core_comm_timer;
 
-//core1 responsible for can bus communication, core0 for most else
+//Communication between cores (core1 responsible for can bus communication, core0 for most else):
 enum inter_core_cmds { 
   //From core1 to core0: contains data read (16 bit)
   ICC_READ_DATA = 1,  
@@ -25,6 +29,13 @@ enum inter_core_cmds {
   ICC_ERROR_DATA = 3
 };
 
+//Can stuff:
+MCP2515 can0 {spi0, 17, 19, 16, 18, 10000000};
+const uint8_t interruptPin {20};
+volatile bool got_irq {false};
+void read_interrupt(uint gpio, uint32_t events) {
+   got_irq = true;
+} 
 enum can_cmds {
   //Wake up
   CAN_AWAKE = 1,
@@ -32,16 +43,18 @@ enum can_cmds {
   CAN_CALIBRATING = 2  
 };
 
+//Node stuff:
 enum node_state {
   NODE_AWAKENING = 1,
   NODE_CALIBRATING = 2,
   NODE_IDLE = 3,
   NODE_DISCUSSING = 4
 };
-
-int node_state = NODE_AWAKENING; 
+int node_state = NODE_AWAKENING;
+bool first_time_idle = true; 
 uint8_t nodes[3] = {0, 0, 0};
 
+//Setup for core0:
 void setup() {
   uint8_t pico_flash_id[8];
   rp2040.idleOtherCore(); 
@@ -53,7 +66,9 @@ void setup() {
 
   analogReadResolution(12); //default is 10
   analogWriteFreq(60000); //60KHz, about max
-  analogWriteRange(DAC_RANGE); //100% duty cycle  
+  analogWriteRange(DAC_RANGE); //100% duty cycle
+
+  //add_repeating_timer_us( -100, core_comm_timer_callback, NULL, &core_comm_timer);  
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
@@ -61,12 +76,7 @@ void setup() {
   delay(1000); 
 }
 
-const uint8_t interruptPin {20};
-volatile bool got_irq {false};
-void read_interrupt(uint gpio, uint32_t events) {
-   got_irq = true;
-}
-
+//Setup for core1:
 void setup1() {
   can0.reset();
   can0.setBitrate(CAN_1000KBPS); 
@@ -75,6 +85,7 @@ void setup1() {
   delay(1000);
 }
 
+//State machine on core0:
 void loop() {
   switch(node_state){
     case NODE_AWAKENING : {
@@ -158,9 +169,6 @@ void loop() {
           }
           delay(500);
           analogWrite(LED_PIN, 0);
-          Serial.println(data.coupling_gains[0]);
-          Serial.println(data.coupling_gains[1]);
-          Serial.println(data.coupling_gains[2]);
         }
         
         if( rp2040.fifo.pop_nb(& msg) ) {
@@ -191,9 +199,15 @@ void loop() {
     }
 
     case NODE_IDLE : {
+      if(first_time_idle){
+        control.init_local_pid(0.02, 1.2, 8, 0.07, 1); //h, K, b, Ti, Tt
+        add_repeating_timer_ms( -10, local_control_timer_callback, NULL, &local_control_timer);
+        first_time_idle = false;
+      }
+      
       while(1){
-        Serial.println("In Idle");
-        delay(2500);
+        handleSerial();
+        delayMicroseconds(10);
       }
 
     }
@@ -207,36 +221,103 @@ void loop() {
   }
 }
 
+//Callbacks for core0:
+bool local_control_timer_callback( struct repeating_timer *t ){
+  int read_adc = analogRead(LDR_PIN);
+  int u = control.compute_local_control(read_adc);
+  analogWrite(LED_PIN, u);
+  return true;
+}
+
+//State machine on core1:
 void loop1() {
-  can_frame frm;
-  uint32_t msg;
-  uint8_t b[4];
-  //reading the can-bus and writing the fifo
-  if(got_irq) { 
-    got_irq = false;
-    uint8_t irq = can0.getInterrupts();
-    if(irq & MCP2515::CANINTF_RX0IF) {
-      can0.readMessage( MCP2515::RXB0, &frm );
-      rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+  switch(node_state){
+    case NODE_AWAKENING : { 
+      while(1){
+        can_frame frm;
+        uint32_t msg;
+        uint8_t b[4];
+        //reading the can-bus and writing the fifo
+        if(got_irq) { 
+          got_irq = false;
+          uint8_t irq = can0.getInterrupts();
+          if(irq & MCP2515::CANINTF_RX0IF) {
+            can0.readMessage( MCP2515::RXB0, &frm );
+            rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+          }
+          if(irq & MCP2515::CANINTF_RX1IF) {
+            can0.readMessage(MCP2515::RXB1, &frm);
+            rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+          }
+          if( can0.checkError()) {
+            uint8_t err = can0.getErrorFlags();
+            rp2040.fifo.push_nb(error_flags_to_msg(irq, err));
+          }
+        }
+        //read fifo and write can-bus
+        if( rp2040.fifo.pop_nb( &msg ) ) {
+          msg_to_bytes( msg , b );
+          if( b[0] == ICC_WRITE_DATA ) {
+            frm.can_id = b[1]; 
+            frm.can_dlc = 2; 
+            frm.data[0] = b[2]; 
+            frm.data[1] = b[3];
+            can0.sendMessage(&frm);
+          }
+        }
+
+        if(node_state != NODE_AWAKENING) break;
+      }
     }
-    if(irq & MCP2515::CANINTF_RX1IF) {
-      can0.readMessage(MCP2515::RXB1, &frm);
-      rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+
+    case NODE_CALIBRATING : {
+      while(1){
+        can_frame frm;
+        uint32_t msg;
+        uint8_t b[4];
+        //reading the can-bus and writing the fifo
+        if(got_irq) { 
+          got_irq = false;
+          uint8_t irq = can0.getInterrupts();
+          if(irq & MCP2515::CANINTF_RX0IF) {
+            can0.readMessage( MCP2515::RXB0, &frm );
+            rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+          }
+          if(irq & MCP2515::CANINTF_RX1IF) {
+            can0.readMessage(MCP2515::RXB1, &frm);
+            rp2040.fifo.push_nb(can_frame_to_msg( &frm ) ); 
+          }
+          if( can0.checkError()) {
+            uint8_t err = can0.getErrorFlags();
+            rp2040.fifo.push_nb(error_flags_to_msg(irq, err));
+          }
+        }
+        //read fifo and write can-bus
+        if( rp2040.fifo.pop_nb( &msg ) ) {
+          msg_to_bytes( msg , b );
+          if( b[0] == ICC_WRITE_DATA ) {
+            frm.can_id = b[1]; 
+            frm.can_dlc = 2; 
+            frm.data[0] = b[2]; 
+            frm.data[1] = b[3];
+            can0.sendMessage(&frm);
+          }
+        }
+
+        if(node_state != NODE_CALIBRATING) break;
+      }
     }
-    if( can0.checkError()) {
-      uint8_t err = can0.getErrorFlags();
-      rp2040.fifo.push_nb(error_flags_to_msg(irq, err));
+
+    case NODE_IDLE : {
+
     }
-  }
-  if( rp2040.fifo.pop_nb( &msg ) ) {//read fifo write bus
-    msg_to_bytes( msg , b );
-    if( b[0] == ICC_WRITE_DATA ) {
-      frm.can_id = b[1]; 
-      frm.can_dlc = 2; 
-      frm.data[0] = b[2]; 
-      frm.data[1] = b[3];
-      can0.sendMessage(&frm);
+
+    case NODE_DISCUSSING : {
+
     }
+
+    default:
+      break;
   }
 }
 
@@ -293,4 +374,22 @@ void print_can_errors(uint8_t canintf, uint8_t eflg) {
     Serial.print("   | "); 
   } 
   Serial.println(".");
+}
+
+void handleSerial(){
+  if(Serial.available()){
+    char cmd = Serial.read();
+    float val;
+    if(Serial.available()){
+      val = Serial.parseInt();
+    }
+    switch(cmd){
+      case 'r':
+        //set lux reference in interval 0 to 100
+        control.lux_ref = val;
+        break;
+      default:
+        break;     
+    }
+  }
 }
