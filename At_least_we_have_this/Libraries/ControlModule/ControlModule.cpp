131a7bb1bce60c013666f7aea012539bcaf08592
Jacob Monrad <77981902+Jacobskode@users.noreply.github.com>
2023-04-19 20:34:14 +0200

    Update ControlModule.cpp

--- At_least_we_have_this/Libraries/ControlModule/ControlModule.cpp
+++ At_least_we_have_this/Libraries/ControlModule/ControlModule.cpp
@@ -17,3 +17,72 @@ int ControlModule::compute_local_control(int adc_val){
     local_pid.housekeep(this->lux_ref, lux);
     return (int)u;
 }
+void ControlModule::writeSendIntentQueue(float* _d){
+    if(mutex_enter_timeout_ms(&sendQueueMtx,10)){
+    for (int i =0; i<3; i++){
+        sendIntentQueue.add(_d[i]);
+    }
+    readyToSendQueue=1;
+    mutex_exit(&sendQueueMtx);
+    }
+}
+float* ControlModule::readSendIntentQueue(){
+    if(mutex_enter_timeout_ms(&sendQueueMtx,10)){
+
+    static float intent[3];
+    for (int i =0;i<3;i++){
+        intent[i]=sendIntentQueue.pop();
+    }
+    readyToSendQueue=0;
+    mutex_exit(&sendQueueMtx);
+    return intent;
+    }else{
+        return nullptr;
+    }
+}
+void ControlModule::writeReceiveIntentQueue(float* _d,int index){
+    if(mutex_enter_timeout_ms(&receiveQueueMtx,10)){
+    for (int i =0; i<3; i++){
+        receiveIntentQueue[index].add(_d[i]);
+    }
+    readyToReadQueue[index]=1;
+    mutex_exit(&receiveQueueMtx);
+    }
+}
+float* ControlModule::readReceiveIntentQueue(int index){
+
+    if(mutex_enter_timeout_ms(&receiveQueueMtx,10)){
+
+    static float intent[3];
+    for (int i =0;i<3;i++){
+        intent[i]=receiveIntentQueue[index].pop();
+    }
+    readyToReadQueue[index]=0;
+    mutex_exit(&receiveQueueMtx);
+    return intent;
+    }else{
+        return nullptr;
+    }
+}
+
+void ControlModule::emptyQueue(int whichQueue){
+
+    if(whichQueue==0){
+        if(mutex_enter_timeout_ms(&receiveQueueMtx,10)){
+        for (int i=0; i<2;i++){
+            while(!receiveIntentQueue[i].is_empty()){
+                receiveIntentQueue[i].pop();
+            }
+            mutex_exit(&receiveQueueMtx);
+        }
+
+        }
+    }else{
+        if(mutex_enter_timeout_ms(&sendQueueMtx,10)){
+        while(!sendIntentQueue.is_empty()){
+            sendIntentQueue.pop();
+        }
+        mutex_exit(&sendQueueMtx);
+        }        
+    }
+}
